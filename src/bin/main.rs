d0f702ab625fb8473558474d344c0c8d6e7b796a
wangbyby <42870190+wangbyby@users.noreply.github.com>
2020-03-25 15:58:39 +0800

    删除无关代码

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,7 +2,6 @@
 use std::io;
 use std::io::prelude::*;
 use monkey::repl::repl;
-// use monkey::parser::test;
 fn main() {
     
     io::stdout().write("Hello Monkey\n".as_bytes()).unwrap();
--- src/data_structures/mod.rs
+++
@@ -1,6 +0,0 @@
-
-pub mod heap;
-pub mod bintree;
-pub mod list;
-pub mod graph;
-pub mod trie; //trie的实现...
