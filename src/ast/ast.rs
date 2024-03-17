c22ece6ece7cf80b6e72a571cb2701c17eddde61
wangboyang <wangboyang6@huawei.com>
2024-03-17 21:33:32 +0800

    nfc

--- src/ast/ast.rs
+++ src/ast/ast.rs
@@ -1,6 +1,5 @@
 use crate::token::token::{self, Token};
 
-
 // use std::any::Any;
 //干...
 // pub trait Node: std::fmt::Debug {
@@ -11,8 +10,8 @@ use crate::token::token::{self, Token};
 //     // fn as_super(&self) ->&dyn Node;//还是不要加上为好
 // }
 
-#[derive(Debug,PartialEq,Clone,Eq,Hash)]
-pub enum ASTNode{
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+pub enum ASTNode {
     Program(Program),
     LetStatement(LetStatement),
     BlockStatement(BlockStatement),
@@ -26,29 +25,28 @@ pub enum ASTNode{
     IntegerLiteral(IntegerLiteral),
     PrefixExpression(PrefixExpression),
     ReturnStatement(ReturnStatement),
-    StringLiteral(token::Token,String),
+    StringLiteral(token::Token, String),
     ArrayLiteral(token::Token, Vec<Box<ASTNode>>),
-    IndexLiteral(token::Token,Box<ASTNode>, Box<ASTNode>), //token, left, index
-    HashLiteral(token::Token,Vec<(Box<ASTNode>, Box<ASTNode>)>),
+    IndexLiteral(token::Token, Box<ASTNode>, Box<ASTNode>), //token, left, index
+    HashLiteral(token::Token, Vec<(Box<ASTNode>, Box<ASTNode>)>),
     None,
 }
 
 //可以为hash的key的类型
 //bool,string, i64,没了?好像没有啦
 
-
-impl ASTNode{
-    pub fn new()-> Self{
+impl ASTNode {
+    pub fn new() -> Self {
         ASTNode::None
     }
-    pub fn is_none(&self) -> bool{
+    pub fn is_none(&self) -> bool {
         use self::ASTNode::None;
-        match self{
-            None=>true,
-            _=>false,
+        match self {
+            None => true,
+            _ => false,
         }
     }
-    pub fn is_some(&self) ->bool{
+    pub fn is_some(&self) -> bool {
         !self.is_none()
     }
 }
@@ -57,130 +55,123 @@ impl ASTNode{
 //     fn as_super(&self) ->&dyn Node {
 //         self
 //     }
-// } 
+// }
 
-#[derive(Debug,PartialEq,Clone,Eq,Hash)]
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
 pub struct Program {
     pub statements: Vec<Box<ASTNode>>,
 }
 
-
 impl Program {
-    pub fn new()->Self{
-        Program{ statements:vec![] }
+    pub fn new() -> Self {
+        Program { statements: vec![] }
     }
-    
-
 }
 
-
-
-#[derive(Debug,PartialEq,Clone,Eq,Hash)]
-pub struct LetStatement{
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+pub struct LetStatement {
     pub token: token::Token,
     pub name: Box<Identifier>,
     pub value: Box<ASTNode>,
 }
 
-impl LetStatement{
-    pub fn new()->LetStatement{
-        LetStatement{
+impl LetStatement {
+    pub fn new() -> LetStatement {
+        LetStatement {
             token: token::Token::default(),
-            name:Box::new(Identifier::default()),
-            value:Box::new(ASTNode::None),
+            name: Box::new(Identifier::default()),
+            value: Box::new(ASTNode::None),
         }
     }
-    pub fn is_none(&self)->bool{
+    pub fn is_none(&self) -> bool {
         self.value.is_none()
     }
 }
 
-
-
-#[derive(Debug,PartialEq,Clone,Eq,Hash)]
-pub struct Identifier{
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+pub struct Identifier {
     pub token: token::Token,
     pub value: String,
 }
 
-impl Identifier{
-    pub fn new(token: token::Token, value: String) -> Self{
-        Self{
-            token,value,
-        }
+impl Identifier {
+    pub fn new(token: token::Token, value: String) -> Self {
+        Self { token, value }
     }
-    
-    pub fn default()->Self{
-        Identifier{
-            token: token::Token::default(), 
-            value: String::default()
+
+    pub fn default() -> Self {
+        Identifier {
+            token: token::Token::default(),
+            value: String::default(),
         }
     }
 }
 
-
-
-#[derive(Debug,PartialEq,Clone,Eq,Hash)]
-pub struct ReturnStatement{
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+pub struct ReturnStatement {
     pub token: token::Token,
     pub return_value: Box<ASTNode>,
 }
 
-impl ReturnStatement{
-    pub fn new(token: token::Token)->Self{
-        ReturnStatement{token: token, return_value:Box::new(ASTNode::None),}
-    }
-}
-
-//Expression 
-
-#[derive(Debug,PartialEq,Clone,Eq,Hash)]
-pub struct ExpressionStatement{
-    token: token::Token,
-    pub expression: Box<ASTNode>,
-}
-
-impl ExpressionStatement{
-    pub fn new(token: token::Token)->Self{
-        ExpressionStatement{token: token, 
-            expression:Box::new(ASTNode::None),
+impl ReturnStatement {
+    pub fn new(token: token::Token) -> Self {
+        ReturnStatement {
+            token: token,
+            return_value: Box::new(ASTNode::None),
         }
     }
 }
 
+//Expression
 
-
-#[derive(Debug,PartialEq,Clone,Eq,Hash)]
-pub struct IntegerLiteral{
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+pub struct ExpressionStatement {
     token: token::Token,
-    pub value: i64,
+    pub expression: Box<ASTNode>,
 }
-impl IntegerLiteral{
-    pub fn new(token: token::Token)->Self{
-        IntegerLiteral{token: token, value:0,}
+
+impl ExpressionStatement {
+    pub fn new(token: token::Token) -> Self {
+        ExpressionStatement {
+            token: token,
+            expression: Box::new(ASTNode::None),
+        }
     }
 }
 
-impl Default for IntegerLiteral{
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+pub struct IntegerLiteral {
+    token: token::Token,
+    pub value: i64,
+}
+impl IntegerLiteral {
+    pub fn new(token: token::Token) -> Self {
+        IntegerLiteral {
+            token: token,
+            value: 0,
+        }
+    }
+}
+
+impl Default for IntegerLiteral {
     fn default() -> Self {
-        IntegerLiteral{
+        IntegerLiteral {
             token: Token::default(),
             value: 0,
         }
     }
 }
 
-
-#[derive(Debug,PartialEq,Clone,Eq,Hash)]
-pub struct PrefixExpression{
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+pub struct PrefixExpression {
     token: token::Token,
     pub operator: String,
     pub right: Box<ASTNode>,
 }
 
-impl PrefixExpression{
-    pub fn new<S: Into<String>>(token: token::Token,operator: S)->Self{
-        PrefixExpression{
+impl PrefixExpression {
+    pub fn new<S: Into<String>>(token: token::Token, operator: S) -> Self {
+        PrefixExpression {
             token: token,
             operator: operator.into(),
             right: Box::new(ASTNode::None),
@@ -188,55 +179,50 @@ impl PrefixExpression{
     }
 }
 
-#[derive(Debug,PartialEq,Clone,Eq,Hash)]
-pub struct InfixExpression{
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+pub struct InfixExpression {
     token: token::Token,
     pub left: Box<ASTNode>,
     pub right: Box<ASTNode>,
     pub operator: String,
 }
 
-impl InfixExpression{
-    pub fn new<S: Into<String>>(token: token::Token, operator:S)->Self{
-        InfixExpression{
+impl InfixExpression {
+    pub fn new<S: Into<String>>(token: token::Token, operator: S) -> Self {
+        InfixExpression {
             token: token,
             operator: operator.into(),
             left: Box::new(ASTNode::None),
-            right:Box::new(ASTNode::None),
+            right: Box::new(ASTNode::None),
         }
     }
 }
 
-
-#[derive(Debug,PartialEq,Clone,Eq,Hash)]
-pub struct Boolean{
-    pub token: token::Token, 
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+pub struct Boolean {
+    pub token: token::Token,
     pub value: bool,
 }
-impl Boolean{
-    pub fn new(token: token::Token, value: bool) ->Self{
-        Boolean{
-                token: token,
-                value:value,
+impl Boolean {
+    pub fn new(token: token::Token, value: bool) -> Self {
+        Boolean {
+            token: token,
+            value: value,
         }
     }
 }
 
-
-
-#[derive(Debug,PartialEq,Clone,Eq,Hash)]
-pub struct IfExpression{
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+pub struct IfExpression {
     token: token::Token,
     pub condition: Box<ASTNode>,
     pub consequence: Box<ASTNode>,
     pub alternative: Box<ASTNode>,
-
 }
 
-
-impl IfExpression{
-    pub fn new(token: token::Token)->Self{
-        IfExpression{
+impl IfExpression {
+    pub fn new(token: token::Token) -> Self {
+        IfExpression {
             token: token,
             condition: Box::new(ASTNode::None),
             consequence: Box::new(ASTNode::None),
@@ -245,57 +231,51 @@ impl IfExpression{
     }
 }
 
-
-#[derive(Debug,PartialEq,Clone,Eq,Hash)]
-pub struct BlockStatement{
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+pub struct BlockStatement {
     token: token::Token,
     pub statements: Vec<Box<ASTNode>>,
 }
 
-impl BlockStatement{
-    pub fn new(token: token::Token)->Self{
-        BlockStatement{
-                token: token, 
-                statements:vec![],
-            }
+impl BlockStatement {
+    pub fn new(token: token::Token) -> Self {
+        BlockStatement {
+            token: token,
+            statements: vec![],
+        }
     }
-
-
 }
 
-
-
-#[derive(Debug,PartialEq,Clone,Eq,Hash)]
-pub struct FuncLiteral{
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+pub struct FuncLiteral {
     token: token::Token,
     pub params: Vec<Option<Box<Identifier>>>,
     pub body: Box<ASTNode>,
 }
 
-impl FuncLiteral{
-    pub fn new(token: token::Token)->Self{
-        FuncLiteral{token: token, params: Vec::new(), body: Box::new(ASTNode::None),}
+impl FuncLiteral {
+    pub fn new(token: token::Token) -> Self {
+        FuncLiteral {
+            token: token,
+            params: Vec::new(),
+            body: Box::new(ASTNode::None),
+        }
     }
-
 }
 
-
-
-
-#[derive(Debug,PartialEq,Clone,Eq,Hash)]
-pub struct CallExpression{
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+pub struct CallExpression {
     token: token::Token,
-    pub func: Box<ASTNode>, //函数名
+    pub func: Box<ASTNode>,      //函数名
     pub args: Vec<Box<ASTNode>>, //函数定义时的参数
 }
 
-impl CallExpression{
-    pub fn new(token: token::Token)->Self{
-        CallExpression{
-            token:token,
+impl CallExpression {
+    pub fn new(token: token::Token) -> Self {
+        CallExpression {
+            token: token,
             func: Box::new(ASTNode::None),
             args: Vec::new(),
         }
     }
 }
-
